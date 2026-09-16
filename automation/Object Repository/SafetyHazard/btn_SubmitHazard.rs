342bf5d6573857ada0f6678c573614677b4aa3a8
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 19:08:23 +0700

    Add Safety Hazard submit test object

---
+++ automation/Object Repository/SafetyHazard/btn_SubmitHazard.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Submit button on Safety Hazard Report form</description>
+   <name>btn_SubmitHazard</name>
+   <tag></tag>
+   <elementGuidId>btn-submit-safety-hazard</elementGuidId>
+
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_SUBMIT_HAZARD_BUTTON_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
