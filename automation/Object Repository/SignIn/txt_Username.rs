afd9d7a12893a87925b16b3a2d52836482504972
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 18:22:02 +0700

    Add username object for sign in

---
+++ automation/Object Repository/SignIn/txt_Username.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Username field on WeMine Sign In page</description>
+   <name>txt_Username</name>
+   <tag></tag>
+   <elementGuidId>txt-username-wemine</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_USERNAME_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
