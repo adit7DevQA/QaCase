ded47328ff11e9686d09d1bba234fe13ef9f1dcf
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 18:42:45 +0700

    Refactor Sign In test to use global variables

--- automation/Test Cases/SignIn/TC_SignIn_Valid.groovy
+++ automation/Test Cases/SignIn/TC_SignIn_Valid.groovy
@@ -2,71 +2,70 @@ import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling
+import internal.GlobalVariable
 
 /*
- * Test Case   : TC_SignIn_Valid
- * Scenario    : Sign In with valid user
- * Type        : Positive
- * Priority    : Critical
+ * Test Case : TC_SignIn_Valid
+ * Scenario  : Sign In with valid user
  *
  * Precondition:
  * - WeMine application is installed
- * - User has valid credentials
- * - User is on the Sign In page
+ * - Valid test credentials are available
  */
 
-Mobile.comment('Start Test Case: Sign In with valid user')
+Mobile.comment('Start Sign In test')
 
-// Start WeMine application
-Mobile.startApplication('PATH_TO_WEMINE_APK', false)
+// Start application
+Mobile.startApplication(GlobalVariable.APP_PATH, false)
 
-// Wait until Sign In page is displayed
+// Wait for Sign In page
 Mobile.waitForElementPresent(
     findTestObject('SignIn/txt_Username'),
     10,
     FailureHandling.STOP_ON_FAILURE
 )
 
-// Enter valid username
+// Input username
 Mobile.setText(
     findTestObject('SignIn/txt_Username'),
-    'VALID_USERNAME',
-    10
-)
-
-// Tap Continue / Sign In button
-Mobile.tap(
-    findTestObject('SignIn/btn_SignIn'),
-    10
-)
-
-// Wait for Microsoft Login page
-Mobile.waitForElementPresent(
-    findTestObject('SignIn/txt_MicrosoftPassword'),
-    10
-)
-
-// Enter valid password
-Mobile.setText(
-    findTestObject('SignIn/txt_MicrosoftPassword'),
-    'VALID_PASSWORD',
+    GlobalVariable.USERNAME,
     10
 )
 
 // Tap Sign In
 Mobile.tap(
+    findTestObject('SignIn/btn_SignIn'),
+    10
+)
+
+// Wait for Microsoft authentication page
+Mobile.waitForElementPresent(
+    findTestObject('SignIn/txt_MicrosoftPassword'),
+    10,
+    FailureHandling.STOP_ON_FAILURE
+)
+
+// Input password
+Mobile.setText(
+    findTestObject('SignIn/txt_MicrosoftPassword'),
+    GlobalVariable.PASSWORD,
+    10
+)
+
+// Tap Microsoft Sign In
+Mobile.tap(
     findTestObject('SignIn/btn_MicrosoftSignIn'),
     10
 )
 
-// Verify user successfully signs in
+// Verify Home page is displayed
 Mobile.waitForElementPresent(
     findTestObject('Home/lbl_Home'),
     30,
     FailureHandling.STOP_ON_FAILURE
 )
 
-Mobile.comment('User successfully signed in to WeMine')
+Mobile.comment('Sign In successful')
 
 // Close application
 Mobile.closeApplication()
