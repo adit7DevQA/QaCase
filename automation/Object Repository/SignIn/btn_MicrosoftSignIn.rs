f359a525329a4dab467faed833c9db2fae0aa329
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 18:30:43 +0700

    Add Microsoft sign in button object

---
+++ automation/Object Repository/SignIn/btn_MicrosoftSignIn.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+    <description>Sign In button on Microsoft authentication page</description>
+    <name>btn_MicrosoftSignIn</name>
+    <tag></tag>
+    <elementGuidId>btn-microsoft-signin-wemine</elementGuidId>
+    <selectorCollection>
+        <entry>
+            <key>XPATH</key>
+            <value>PLACEHOLDER_MICROSOFT_SIGNIN_BUTTON_LOCATOR</value>
+        </entry>
+    </selectorCollection>
+    <selectorMethod>XPATH</selectorMethod>
+    <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
