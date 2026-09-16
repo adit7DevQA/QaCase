9e7ef771c7166f535e14991e53e5c89c53605d4f
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 18:38:53 +0700

    Add home verification object

---
+++ automation/Object Repository/Home/lbl_Home.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+    <description>Home element displayed after successful Sign In</description>
+    <name>lbl_Home</name>
+    <tag></tag>
+    <elementGuidId>lbl-home-wemine</elementGuidId>
+    <selectorCollection>
+        <entry>
+            <key>XPATH</key>
+            <value>PLACEHOLDER_HOME_LOCATOR</value>
+        </entry>
+    </selectorCollection>
+    <selectorMethod>XPATH</selectorMethod>
+    <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
