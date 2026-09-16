9d555d95fab6cdf3cb392549f52ff796a37bdb6c
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 19:09:33 +0700

    Add Safety Hazard success test object

---
+++ automation/Object Repository/SafetyHazard/lbl_HazardSubmitSuccess.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Success message displayed after Safety Hazard Report is submitted</description>
+   <name>lbl_HazardSubmitSuccess</name>
+   <tag></tag>
+   <elementGuidId>lbl-safety-hazard-submit-success</elementGuidId>
+
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_HAZARD_SUBMIT_SUCCESS_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
