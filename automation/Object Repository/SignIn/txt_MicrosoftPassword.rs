de1e88e7bc2ad42e5518bc05b46db83f9004cc5c
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 18:28:57 +0700

    Add Microsoft password object

---
+++ automation/Object Repository/SignIn/txt_MicrosoftPassword.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+    <description>Password field on Microsoft Sign In page</description>
+    <name>txt_MicrosoftPassword</name>
+    <tag></tag>
+    <elementGuidId>txt-microsoft-password-wemine</elementGuidId>
+    <selectorCollection>
+        <entry>
+            <key>XPATH</key>
+            <value>PLACEHOLDER_MICROSOFT_PASSWORD_LOCATOR</value>
+        </entry>
+    </selectorCollection>
+    <selectorMethod>XPATH</selectorMethod>
+    <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
