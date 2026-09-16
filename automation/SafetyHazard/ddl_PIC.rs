0a3a4ff13a58862edccd2fc177a55cd7337ffc3c
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 19:21:23 +0700

    Add Safety Hazard PIC test object

---
+++ automation/SafetyHazard/ddl_PIC.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>PIC dropdown on Safety Hazard Report form</description>
+   <name>ddl_PIC</name>
+   <tag></tag>
+   <elementGuidId>ddl-safety-hazard-pic</elementGuidId>
+
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_HAZARD_PIC_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
