2e22e7a84eca3b8928a30879ebc3555d8b647359
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 19:07:07 +0700

    Add Safety Hazard description test object

---
+++ automation/Object Repository/SafetyHazard/txt_HazardDescription.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Description field on Safety Hazard Report form</description>
+   <name>txt_HazardDescription</name>
+   <tag></tag>
+   <elementGuidId>txt-safety-hazard-description</elementGuidId>
+
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_HAZARD_DESCRIPTION_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
