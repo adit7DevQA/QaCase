035df20fba3eb3a64b41c317f52683c69b438078
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 19:02:43 +0700

    Add invalid credential error test object

---
+++ automation/Test Cases/SignIn/lbl_InvalidCredentialError.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Error message displayed when Sign In credentials are invalid</description>
+   <name>lbl_InvalidCredentialError</name>
+   <tag></tag>
+   <elementGuidId>lbl-invalid-credential-error-wemine</elementGuidId>
+
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_INVALID_CREDENTIAL_ERROR_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
