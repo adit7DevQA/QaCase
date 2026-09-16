f68014058160e7765368625a8069a4b91a389584
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 19:20:31 +0700

    Add Safety Hazard evidence test object

---
+++ automation/SafetyHazard/img_Evidence.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Evidence image picker on Safety Hazard Report form</description>
+   <name>img_Evidence</name>
+   <tag></tag>
+   <elementGuidId>img-safety-hazard-evidence</elementGuidId>
+
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_HAZARD_EVIDENCE_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
