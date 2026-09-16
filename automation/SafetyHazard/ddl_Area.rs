0de2ce8a5191555f5b6fd168e27a60f4b89bb3fe
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 19:19:56 +0700

    Add Safety Hazard area test object

---
+++ automation/SafetyHazard/ddl_Area.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Area dropdown on Safety Hazard Report form</description>
+   <name>ddl_Area</name>
+   <tag></tag>
+   <elementGuidId>ddl-safety-hazard-area</elementGuidId>
+
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_HAZARD_AREA_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
