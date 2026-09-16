e3ca7f0a0778d917eb3410f73f1258ab055250ad
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 19:18:10 +0700

    Add Safety Hazard location test object

---
+++ automation/SafetyHazard/ddl_Location.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Location dropdown on Safety Hazard Report form</description>
+   <name>ddl_Location</name>
+   <tag></tag>
+   <elementGuidId>ddl-safety-hazard-location</elementGuidId>
+
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_HAZARD_LOCATION_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
