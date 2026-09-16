be319cae01ec9e7974fce26ca3b1ccb94e6b9340
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 19:18:58 +0700

    Add Safety Hazard sublocation test object

---
+++ automation/SafetyHazard/ddl_Sublocation.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Sublocation dropdown on Safety Hazard Report form</description>
+   <name>ddl_Sublocation</name>
+   <tag></tag>
+   <elementGuidId>ddl-safety-hazard-sublocation</elementGuidId>
+
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_HAZARD_SUBLOCATION_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
