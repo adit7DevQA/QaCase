34b49e2028560a1541a73205ca79370536c69063
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 18:23:36 +0700

    Add sign in button object

---
+++ automation/Object Repository/SignIn/btn_SignIn.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+    <description>Sign In button on WeMine Sign In page</description>
+    <name>btn_SignIn</name>
+    <tag></tag>
+    <elementGuidId>btn-signin-wemine</elementGuidId>
+    <selectorCollection>
+        <entry>
+            <key>XPATH</key>
+            <value>PLACEHOLDER_SIGNIN_BUTTON_LOCATOR</value>
+        </entry>
+    </selectorCollection>
+    <selectorMethod>XPATH</selectorMethod>
+    <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
