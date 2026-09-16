f083aa30766b51c2c7f08f75f9bfa8620e4b06f0
Aditya Juda Manggala <157895263+adit7DevQA@users.noreply.github.com>
2026-09-16 19:05:49 +0700

    Add create Safety Hazard test object

---
+++ automation/Object Repository/SafetyHazard/btn_CreateHazard.rs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Button to create a new Safety Hazard Report</description>
+   <name>btn_CreateHazard</name>
+   <tag></tag>
+   <elementGuidId>btn-create-safety-hazard</elementGuidId>
+
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>PLACEHOLDER_CREATE_HAZARD_BUTTON_LOCATOR</value>
+      </entry>
+   </selectorCollection>
+
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
